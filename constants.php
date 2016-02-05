e3bc7c7cdde75cb879afd74e370b08dbd3717038
kallef <prhost@prhost.com.br>
2016-02-05 13:16:46 -0200

    general fix

--- constants.php
+++ constants.php
@@ -6,19 +6,19 @@ define('ENVIRONMENT', IS_ADMIN ? 'admin' : 'catalog');
 //get domain name
 define('DOMAINNAME', isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : null);
 
-//define opencart directory
-define('DIR_CORE', realpath(DIR_ROOT . '/opencart/'));
+//define core directory
+define('DIR_CORE', realpath(DIR_ROOT . '/core/'));
 
 $base_url = DOMAINNAME . $_SERVER['SCRIPT_NAME'];
-$base_url = str_replace(['/opencart/admin/', '/opencart', '/index.php'], '/', $base_url);
+$base_url = str_replace(['/core/admin/', '/core', '/index.php'], '/', $base_url);
 define('BASEURL', $base_url);
 
 // HTTP
-define('HTTP_SERVER', 'http://' . BASEURL . 'opencart/admin/');
+define('HTTP_SERVER', 'http://' . BASEURL . 'core/admin/');
 define('HTTP_CATALOG', 'http://' . BASEURL);
 
 // HTTPS
-define('HTTPS_SERVER', 'https://' . BASEURL . 'opencart/admin/');
+define('HTTPS_SERVER', 'https://' . BASEURL . 'core/admin/');
 define('HTTPS_CATALOG', 'http://' . BASEURL);
 
 // DIR
@@ -32,7 +32,8 @@ define('DIR_APPLICATION', DIR_CORE . '/' . ENVIRONMENT . '/');
 define('DIR_SYSTEM', DIR_CORE . '/system/');
 define('DIR_STORAGE', DIR_ROOT . '/storage');
 define('DIR_LANGUAGE', DIR_CORE . '/' . ENVIRONMENT . '/language/');
-define('DIR_CONFIG', DIR_CORE . '/config/');
+define('DIR_CONFIG', DIR_ROOT . '/config/');
+define('PATH_IMAGE', 'storage/image/');
 define('DIR_IMAGE', DIR_STORAGE . '/image/');
 define('DIR_CACHE', DIR_STORAGE . '/cache/');
 define('DIR_DOWNLOAD', DIR_STORAGE . '/download/');
@@ -40,3 +41,4 @@ define('DIR_UPLOAD', DIR_STORAGE . '/upload/');
 define('DIR_MODIFICATION', DIR_STORAGE . '/modification/');
 define('DIR_LOGS', DIR_STORAGE . '/logs/');
 define('DIR_CATALOG', DIR_CORE . '/catalog/');
+define('DIR_VQMOD_CACHE', DIR_STORAGE . '/vqmod/');
